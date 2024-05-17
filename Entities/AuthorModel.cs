f408985c7f33aa605d586d168689764b7e145379
Abhilash S <abhilash.b@sysfore.com>
2024-05-17 12:43:25 +0530

    Created "AuthorModel" class

---
+++ Entities/AuthorModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class AuthorModel
+    {
+        [Key]
+        public Guid AuthorID { get; set; }
+        public AuthorName Name { get; set; }
+        public string Biography { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Country { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public List<BookAuthor> BookAuthor { get; set; }
+
+    }
+}
---
+++ Entities/AuthorName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    public class AuthorName
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
