aa6385f74941e0c7671a15a5053dc4ed6a88d25c
Abhilash S <abhilash.b@sysfore.com>
2024-05-17 12:52:52 +0530

    Implemented DbContext

---
+++ Ebook.Data/ApplicationDBContext.cs
@@ -0,0 +1,40 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ebook.Data
+{
+    public class ApplicationDBContext : DbContext 
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AuthorModel>()
+                .OwnsOne(author => author.Name);
+
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(book => book.Book)
+                .WithMany(bookauthor => bookauthor.BookAuthor)
+                .HasForeignKey(bookid => bookid.BookId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(author => author.Author)
+                .WithMany(bookauthor => bookauthor.BookAuthor)
+                .HasForeignKey(authorid => authorid.AuthorId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(m => new { m.BookId, m.AuthorId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<LoginRequest> EFCCredentials { get; set; }
+        public DbSet<AuthorModel> EFCAuthor { get; set; }
+        public DbSet<BookModel> EFCBooks { get; set; }
+        public DbSet<BookAuthor> EFCBookAuthor { get; set; }
+    }
+}
