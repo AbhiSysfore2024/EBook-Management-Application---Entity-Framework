a140693b8b9a2f11982c7aef93930e15149346ba
Abhilash S <abhilash.b@sysfore.com>
2024-05-17 12:44:16 +0530

    Implemented "BookAuthor" class

---
+++ Entities/BookAuthor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class BookAuthor
+    {
+        [Column(Order = 0)]
+        public Guid BookId { get; set; }
+
+        [Column(Order = 1)]
+        public Guid AuthorId { get; set; }
+
+        public virtual BookModel Book { get; set; }
+
+        public virtual AuthorModel Author { get; set; }
+    }
+
+}
