ee4fe89c6faccccec166bf65c1994e98aa7d73c4
Abhilash S <abhilash.b@sysfore.com>
2024-05-21 14:12:14 +0530

    Implemented custom exceptions

---
+++ CustomExceptions/Exceptions.cs
@@ -0,0 +1,29 @@
+﻿namespace CustomExceptions
+{
+    public class EmptOrNullSpaceUsrnmePsswrd : Exception
+    {
+        public EmptOrNullSpaceUsrnmePsswrd(string message) : base(message) { }
+
+    }
+
+    public class EqualUserNameAndPassword : Exception
+    {
+        public EqualUserNameAndPassword(string message) : base(message) { }
+    }
+
+    public class NotUniqueUserName : Exception
+    {
+        public NotUniqueUserName(string message) : base(message) { }
+    }
+
+    public class AuthorNotFound : Exception
+    {
+        public AuthorNotFound(string message) : base(message) { }
+    }
+
+    public class BookNotFound : Exception
+    {
+        public BookNotFound(string message) : base(message) { }
+    }
+
+}
