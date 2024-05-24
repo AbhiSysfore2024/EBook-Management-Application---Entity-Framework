251d75b7b3978390b417b301f00603deab68b73e
Abhilash S <abhilash.b@sysfore.com>
2024-05-24 16:15:05 +0530

    Handling JSON serilization deserialization cycle

--- EBook Management Application - Entity Framework/Program.cs
+++ EBook Management Application - Entity Framework/Program.cs
@@ -1,15 +1,25 @@
 using AppSettings;
 using Ebook.Data;
+using EBook_Management_Application___Entity_Framework.Extensions;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerManagementStudio")));
+builder.Services.Configure<JWTClaimDetails>(builder.Configuration.GetSection("Jwt"));
+builder.Services.SingletonService();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.SwaggerAuthorization();
 
 var app = builder.Build();
 
@@ -22,6 +32,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
